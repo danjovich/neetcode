f0ff088c872d24dd547d621bd2f009fb661aff66
Daniel <78160019+danjovich@users.noreply.github.com>
2026-09-15 01:41:27 -0300

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.py
@@ -0,0 +1,47 @@
+from collections import defaultdict, deque
+from heapq import heappop, heappush
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for i, (a, b) in enumerate(edges):
+            graph[a].append((b, i))
+            graph[b].append((a, i))
+
+        print(graph)
+
+        # q = deque([edges[0][0]])
+        # visited = {edges[0][0]}
+        # cicle_start = -1
+        # while q:
+        #     node = q.popleft()
+
+        #     for neighbor in graph[node]:
+        #         if neighbor in visited:
+        #             cicle_start = neighbor
+        #         else:
+        #             heappush(h, (-i, neighbor))
+        #             visited.add(neighbor)
+
+        res = -1
+        visited_nodes, visited_edges = set(), set()
+
+        def dfs(node: int) -> bool:
+            nonlocal res
+            visited_nodes.add(node)
+
+            found_loop = False
+            for neighbor, i in graph[node]:
+                if i in visited_edges:
+                    continue
+
+                visited_edges.add(i)
+                if neighbor in visited_nodes or dfs(neighbor):
+                    res = max(i, res)
+                    found_loop = True
+
+            return found_loop
+
+        dfs(edges[0][0])
+        return edges[res]
